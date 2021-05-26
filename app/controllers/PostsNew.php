f0d79d37eda41c920cf4fa0328bcec956e0af344
Igor Makhinich <igor.makhinich@gmail.com>
2021-05-26 12:44:21 +0300

    add files

---
+++ app/controllers/PostsNew.php
@@ -0,0 +1,23 @@
+<?php
+class PostsNew
+{
+    public function indexAction()
+    {
+        echo "PostsNew::index";
+    }
+
+    public function testAction()
+    {
+        echo "PostNew::test";
+    }
+
+    public function testPageAction()
+    {
+        echo "PostNew::testPage";
+    }
+
+    public function before()
+    {
+        echo "PostsNew::before";
+    }
+}
---
+++ vendor/core/Router.php
@@ -0,0 +1,96 @@
+<?php
+class Router
+{
+    // Route table
+    // @var array
+    protected static $routes = [];
+
+    // Current route
+    // @var array
+    protected static $route = [];
+
+    // Adding a route to the route table
+    // @param string $regexp регулярное выражение маршрута
+    // @param array $route маршрут ([controller, action, params])
+    public static function add($regexp, $route = [])
+    {
+        self::$routes[$regexp] = $route;
+    }
+
+    // Returns the route table
+    // @return array
+    public static function getRoutes()
+    {
+        return self::$routes;
+    }
+
+    // Returns the current route (controller, action, [params])
+    // @return array
+    public static function getRoute()
+    {
+        return self::$route;
+    }
+
+    // Search for a URL in the route table
+    // @param string $url входящий URL
+    // @return boolean
+    public static function matchRoute($url)
+    {
+        foreach (self::$routes as $pattern => $route) {
+            if (preg_match("#$pattern#i", $url, $matches)) {
+                debug($matches);
+                foreach ($matches as $key => $value) {
+                    if (is_string($key)) {
+                        $route[$key] = $value;
+                    }
+                }
+                if (!isset($route['action'])) {
+                    $route['action'] = 'index';
+                }
+                self::$route = $route;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Redirect URL to the correct route
+    // @param string $url входящий URL
+    // return void
+    public static function dispatch($url)
+    {
+        if (self::matchRoute($url)) {
+            // $controller = self::$route['controller'];
+            // self::upperCamelCase($controller);
+            $controller = self::upperCamelCase(self::$route['controller']);
+            if (class_exists($controller)) {
+                $cObj = new $controller;
+                $action = self::lowerCamelCase(self::$route['action']) . 'Action';
+                debug($action);
+                if (method_exists($cObj, $action)) {
+                    $cObj->$action();
+                } else {
+                    echo "Method <b>$controller::$action</b> not found";
+                }
+            } else {
+                echo "Controller <b>$controller</b> not found";
+            }
+        } else {
+            http_response_code(404);
+            include '404.html';
+        }
+    }
+
+    protected static function upperCamelCase($name)
+    {
+        // $name = str_replace('-',' ', $name);
+        // $name = ucwords($name);
+        // $name = str_replace(' ','', $name);
+        return str_replace(' ', '', ucwords(str_replace('-', ' ', $name)));
+    }
+
+    protected static function lowerCamelCase($name)
+    {
+        return lcfirst(str_replace(' ', '', ucwords(str_replace('-', ' ', $name))));
+    }
+}
